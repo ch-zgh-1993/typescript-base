2da71f26e2cbec7eaac148275c30c0adf3378294
zhangguohua <zhangguohua@shuidihuzhu.com>
2019-09-03 21:04:44 +0800

    update_泛型_end

--- TestProgram/js/泛型.js
+++ TestProgram/js/泛型.js
@@ -12,3 +12,42 @@ let myIdentity1 = identity1;
 // 泛型类
 class GenericNumber {
 }
+let myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+// 在泛型里使用类类型：
+// 使用原型属性推断并约束构造函数与类实例的关系
+// 作用？？
+class BeeKeeper {
+    constructor() {
+        this.hasMask = true;
+    }
+}
+class ZooKeeper {
+    constructor() {
+        this.nametag = 'zoo';
+    }
+}
+class Animal1 {
+    constructor() {
+        this.numLegs = 1;
+    }
+}
+class Bee extends Animal1 {
+    constructor() {
+        super();
+        this.keeper = new BeeKeeper();
+    }
+}
+class Lion extends Animal1 {
+    constructor() {
+        super();
+        this.keeper = new ZooKeeper();
+    }
+}
+function createInstance(c) {
+    return new c();
+}
+console.log(createInstance(Lion).keeper.nametag);
+;
+console.log(createInstance(Bee).keeper.hasMask);
--- TestProgram/ts/泛型.ts
+++ TestProgram/ts/泛型.ts
@@ -26,3 +26,53 @@ class GenericNumber<T> {
 	zeroValue: T;
 	add: (x: T, y: T) => T;
 }
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x, y){return x + y};
+
+
+// 在泛型里使用类类型：
+// 使用原型属性推断并约束构造函数与类实例的关系
+// 作用？？
+
+class BeeKeeper {
+	hasMask: Boolean;
+	constructor(){
+		this.hasMask = true;
+	}
+}
+
+class ZooKeeper {
+	nametag: string;
+	constructor() {
+		this.nametag = 'zoo';
+	}
+}
+class Animal1 {
+	numLegs: number;
+	constructor() {
+		this.numLegs = 1;
+	}
+}
+class Bee extends Animal1 {
+	keeper: BeeKeeper;
+	constructor() {
+		super();
+		this.keeper = new BeeKeeper();
+	}
+}
+class Lion extends Animal1 {
+	keeper: ZooKeeper;
+	constructor() {
+		super();
+		this.keeper = new ZooKeeper();
+	}
+}
+
+function createInstance<A extends Animal1>(c: new() => A): A{
+	return new c();
+}
+
+console.log(createInstance(Lion).keeper.nametag);;
+console.log(createInstance(Bee).keeper.hasMask);
