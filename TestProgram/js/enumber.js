e8242eec97cc8bfd45b0654b1cf4c467e56c0f04
zhangguohua <zhangguohua@shuidihuzhu.com>
2019-09-09 21:20:05 +0800

    add enumber

---
+++ TestProgram/js/enumber.js
@@ -0,0 +1,20 @@
+"use strict";
+// 枚举
+// 数字枚举
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
+})(Direction || (Direction = {}));
+console.log(Direction.Down);
+// 字符串枚举
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "Up";
+    Direction1["Down"] = "Down";
+    Direction1["Left"] = "Left";
+    Direction1["Right"] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1);
---
+++ TestProgram/ts/enumber.ts
@@ -0,0 +1,19 @@
+// 枚举
+
+// 数字枚举
+enum Direction {
+	Up = 1,
+	Down,
+	Left,
+	Right
+}
+console.log(Direction.Down)
+
+// 字符串枚举
+enum Direction1 {
+	Up = 'Up',
+	Down = 'Down',
+	Left = 'Left',
+	Right = 'Right'
+}
+console.log(Direction1);
