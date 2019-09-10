fd01095fff8e90c44b6778b30d0f722b729b8f24
zhangguohua <zhangguohua@shuidihuzhu.com>
2019-09-10 22:06:04 +0800

    end enumber

--- TestProgram/js/enumber.js
+++ TestProgram/js/enumber.js
@@ -15,6 +15,33 @@ var Direction1;
     Direction1["Up"] = "Up";
     Direction1["Down"] = "Down";
     Direction1["Left"] = "Left";
-    Direction1["Right"] = "Right";
+    Direction1["Right"] = "Right1";
 })(Direction1 || (Direction1 = {}));
 console.log(Direction1);
+// 计算和常量
+// 常量在编译时会进行计算，计算的值直到运行时才知道值。
+var FileAccess;
+(function (FileAccess) {
+    // constant members
+    FileAccess[FileAccess["None"] = 0] = "None";
+    FileAccess[FileAccess["Read"] = 2] = "Read";
+    FileAccess[FileAccess["Write"] = 4] = "Write";
+    FileAccess[FileAccess["ReadWrite"] = 6] = "ReadWrite";
+    // computed member
+    FileAccess[FileAccess["G"] = '123'.length] = "G";
+})(FileAccess || (FileAccess = {}));
+let directions = [0 /* Up */, 1 /* Down */, 2 /* Left */, 3 /* Right */];
+// 内联
+var Foo1;
+(function (Foo1) {
+    Foo1[Foo1["X"] = 4] = "X";
+})(Foo1 || (Foo1 = {}));
+var y = Foo1.X;
+// 外部枚举:  为什么没有编译？
+var Enum1;
+(function (Enum1) {
+    Enum1[Enum1["A"] = 1] = "A";
+    Enum1[Enum1["B"] = 2] = "B";
+    Enum1[Enum1["C"] = 2] = "C";
+})(Enum1 || (Enum1 = {}));
+console.log(Enum1);
--- TestProgram/ts/enumber.ts
+++ TestProgram/ts/enumber.ts
@@ -14,6 +14,48 @@ enum Direction1 {
 	Up = 'Up',
 	Down = 'Down',
 	Left = 'Left',
-	Right = 'Right'
+	Right = 'Right1'
 }
 console.log(Direction1);
+
+// 计算和常量
+// 常量在编译时会进行计算，计算的值直到运行时才知道值。
+enum FileAccess {
+	// constant members
+	None,
+	Read = 1 << 1,
+	Write = 1 << 2,
+	ReadWrite = Read | Write,
+	// computed member
+	G = '123'.length,
+}
+
+// const 枚举
+const enum Direction2 {
+	Up,
+	Down,
+	Left,
+	Right
+}
+let directions = [Direction2.Up, Direction2.Down, Direction2.Left, Direction2.Right]
+// 内联
+enum Foo1 {
+	X = 4,
+}
+var y = Foo1.X
+
+
+// 外部枚举:  为什么没有编译？
+enum Enum1 {
+	A = 1,
+	B,
+	C = 2
+}
+console.log(Enum1)
+
+declare enum Foo {
+    X, // Computed
+    Y = 2, // Non-computed
+    Z, // Computed! Not 3! Careful!
+    Q = 1 + 1 // Error
+}
